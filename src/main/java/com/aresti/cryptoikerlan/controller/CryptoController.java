72c757b729ae1662f6f3b867f0f774372024582e
Xabier Aresti <xabier@aresti.eus>
2022-11-11 03:35:10 +0100

    generateCACertificate created

--- src/main/java/com/aresti/cryptoikerlan/controller/CryptoController.java
+++ src/main/java/com/aresti/cryptoikerlan/controller/CryptoController.java
@@ -3,25 +3,30 @@ package com.aresti.cryptoikerlan.controller;
 import com.aresti.cryptoikerlan.pojo.RootCertificateAndKeyPair;
 import com.aresti.cryptoikerlan.requestsAndResponses.CN;
 import com.aresti.cryptoikerlan.requestsAndResponses.CRT;
+import com.aresti.cryptoikerlan.requestsAndResponses.CSR;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.BasicConstraints;
 import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sun.misc.BASE64Encoder;
 import sun.security.provider.X509Factory;
 
 
-import javax.xml.ws.Response;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.math.BigInteger;
@@ -44,12 +49,46 @@ public class CryptoController {
 
 
     @PostMapping("/ca")
-    public ResponseEntity<CRT> generateCACertificate(@RequestBody CN cm) throws CertificateException, NoSuchAlgorithmException, IOException, SignatureException, NoSuchProviderException, InvalidKeyException, OperatorCreationException {
-        System.out.println("Received CN: " + cm.getCommon_name());
-        storedRootCertificateAndKeyPair = generateCACertificate(cm.getCommon_name());
+    public ResponseEntity<CRT> generateCACertificateAPIPost(@RequestBody CN cn) throws CertificateException, NoSuchAlgorithmException, IOException, SignatureException, NoSuchProviderException, InvalidKeyException, OperatorCreationException {
+        System.out.println("Received CN: " + cn.getCommon_name());
+        storedRootCertificateAndKeyPair = generateCACertificate(cn.getCommon_name());
         CRT crtResponse = new CRT(serializePEMCertificate(storedRootCertificateAndKeyPair.getCertificate()));
         return ResponseEntity.ok(crtResponse);
     }
+
+    @PostMapping
+    public ResponseEntity<CRT> issueCertificateAPIPost(@RequestBody CSR csr){
+        if (storedRootCertificateAndKeyPair != null){
+
+        }
+    }
+
+    private X509Certificate issueCertificate(PKCS10CertificationRequest csr) throws NoSuchAlgorithmException, CertIOException, CertificateException, OperatorCreationException {
+        // Set random rootSerialNumber
+        BigInteger issuedSerialNumber = new BigInteger(Long.toString(new SecureRandom().nextLong()));
+
+        // Set validity - 1 year
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, 1);
+
+        // Get rootIssuer name from rootCert and setup certificate builder
+        X500Name rootCertificateIssuer = new X500Name(storedRootCertificateAndKeyPair.getCertificate().getSubjectX500Principal().getName());
+        X509v3CertificateBuilder issuedCertificateBuilder = new X509v3CertificateBuilder(rootCertificateIssuer, issuedSerialNumber, new Date(), calendar.getTime(), csr.getSubject(), csr.getSubjectPublicKeyInfo());
+
+        // Add basicConstraint to mark it is not a CA certificate and add Issues identifier
+        JcaX509ExtensionUtils issuedCertificateExtensionUtils = new JcaX509ExtensionUtils();
+        issuedCertificateBuilder.addExtension(Extension.basicConstraints, true, new BasicConstraints(false));
+        issuedCertificateBuilder.addExtension(Extension.authorityKeyIdentifier, false, issuedCertificateExtensionUtils.createAuthorityKeyIdentifier(storedRootCertificateAndKeyPair.getCertificate()));
+        issuedCertificateBuilder.addExtension(Extension.subjectKeyIdentifier, false, issuedCertificateExtensionUtils.createSubjectKeyIdentifier(csr.getSubjectPublicKeyInfo()));
+
+        // Identify Algorithm
+        AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder().find(SIG_ALGORITHM);
+        AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder().find(sigAlgId);
+
+        // Create and return X509Certificate
+        return new JcaX509CertificateConverter().setProvider(BC_PROVIDER).getCertificate(issuedCertificateBuilder.build(new JcaContentSignerBuilder(SIG_ALGORITHM).setProvider(BC_PROVIDER).build(storedRootCertificateAndKeyPair.getKeyPair().getPrivate())));
+    }
+
     private RootCertificateAndKeyPair generateCACertificate(String commonName) throws NoSuchAlgorithmException, NoSuchProviderException, IOException, CertificateException, SignatureException, InvalidKeyException, OperatorCreationException {
         // Add the BouncyCastle Provider
         Security.addProvider(new BouncyCastleProvider());
@@ -72,7 +111,7 @@ public class CryptoController {
 
         X509v3CertificateBuilder rootCertificateBuilder = new JcaX509v3CertificateBuilder(rootCertificateIssuer, rootSerialNumber, new Date(), calendar.getTime(), rootCertificateSubject, rootKeyPair.getPublic());
 
-        // Add basicConstraint as CA certificate
+        // Add basicConstraint as CA certificate mark
         JcaX509ExtensionUtils rootCertificateExtensionUtils = new JcaX509ExtensionUtils();
         rootCertificateBuilder.addExtension(Extension.basicConstraints, true, new BasicConstraints(true));
         rootCertificateBuilder.addExtension(Extension.subjectKeyIdentifier, false, rootCertificateExtensionUtils.createSubjectKeyIdentifier(rootKeyPair.getPublic()));
@@ -92,7 +131,7 @@ public class CryptoController {
         printStream.println(X509Factory.END_CERT);
         printStream.flush();
         System.out.println(byteOutputStream);
-        return String.valueOf(byteOutputStream);
+        return byteOutputStream.toString();
     }
 
 
